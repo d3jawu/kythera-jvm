2d795610c46107a4e0b907e12ad29e965e84d71d
Holly <holly@dejawu.me>
2021-10-10 23:42:18 -0400

    Runtime value constructors allowing self-reference using lambdas

--- src/me/dejawu/kythera/Runtime.kt
+++ src/me/dejawu/kythera/Runtime.kt
@@ -1,48 +1,95 @@
 package me.dejawu.kythera;
 
-typealias FieldVals = Map<String, KVal<*>>;
+import java.util.function.Function;
 
-// structs and types are actually the same data type (types are just structs that describe other structs)
-typealias StructTypeKVal = KVal<FieldVals>
+typealias FieldVals = Map<String, KVal<*>>
+
+typealias FnVal = Function<List<KVal<*>>, KVal<*>>
+typealias FnKVal = KVal<FnVal>
+
+typealias FnTypeVal = Pair<List<KVal<*>>, KVal<*>>
+typealias FnTypeKVal = KVal<FnTypeVal>
 
 class KVal<T> {
     private val value: T
-    private val typeValue: StructTypeKVal
+    private var typeValue: KVal<*>
     private val fieldVals: FieldVals;
 
-    constructor(value: T, typeValue: StructTypeKVal, fieldVals: FieldVals) {
-        this.value = value;
-        this.typeValue = typeValue;
-        this.fieldVals = fieldVals;
+    /*
+    constructor(value: T, typeValue: KVal<*>, fieldVals: FieldVals) {
+        this.value = value
+        this.typeValue = typeValue
+        this.fieldVals = fieldVals
     }
+    */
+
+    private constructor(value: T, typeValue: KVal<*>, fieldVals: FieldVals)
 
     // for structs/types, the value *is* the field values
-    constructor(typeValue: StructTypeKVal, fieldVals: FieldVals) {
+    private constructor(typeValue: KVal<*>, fieldVals: FieldVals) {
         this.fieldVals = fieldVals;
-        this.value = this.fieldVals as T;
+        this.value = this.fieldVals as T
 
         this.typeValue = typeValue;
     }
 
-    private constructor(root: T) {
-        this.value = root;
-        this.typeValue = this as StructTypeKVal;
-        this.fieldVals = root as FieldVals;
+    // used only for root type, which references itself for its type-value
+    private constructor(fieldValsFrom: Function1<KVal<*>, FieldVals>) {
+        this.fieldVals = fieldValsFrom(this)
+        this.value = this.fieldVals as T
+
+        this.typeValue = this
+    }
+
+    // allow custom self-referential mapping
+    private constructor(typeValue: KVal<*>, fieldValsFrom: Function1<KVal<*>, FieldVals>) {
+        this.fieldVals = fieldValsFrom(this)
+        this.value = this.fieldVals as T
+
+        this.typeValue = typeValue;
     }
 
     companion object {
+        // stores canonical references for type-values
+        private val typePool = HashMap<KVal<*>, KVal<*>>()
+
+        @JvmStatic
+        // fetches canonical reference for type-value, creating one in the pool if necessary
+        fun getType(value: KVal<*>): KVal<*> {
+            val canonType = typePool[value];
+
+            return if(canonType == null) {
+                typePool[value] = value
+                value
+            } else {
+                typePool[value] ?: throw Exception()
+            }
+        }
+
+        val fnTypeFields: FieldVals = mapOf()
+
         // type literals
         @JvmField
         // root type describes the fields that type-values have, and points to itself for its type value
-        val rootType: StructTypeKVal = KVal(mapOf())
+        val TYPE: KVal<*> = getType(KVal<FieldVals> { TYPE ->
+            mapOf(
+                "<:" to getType(
+                    KVal<FnTypeVal>(
+                        TYPE,
+                        KVal(TYPE, Pair(listOf(TYPE, TYPE), TYPE)),
+                        fnTypeFields
+                    )
+                )
+            )
+        })
 
         @JvmField
-        val intType = StructTypeKVal(
-            rootType,
-            mapOf(
-                // describe int field types here
-            ),
-        )
+//        val INT: KVal<*> = getType(
+//            KVal(TYPE) {
+//                mapOf()
+//            }
+//        )
+
 
         val intFields: FieldVals = mapOf(
             // implement int functions here
@@ -50,11 +97,11 @@ class KVal<T> {
 
         // value factories
         @JvmStatic
-        fun makeInt(value: Int): KVal<Int> {
-            return KVal(
-                value, intType,
-                intFields
-            )
-        }
+//        fun makeInt(value: Int): KVal<Int> {
+//            return KVal(
+//                value, INT,
+//                intFields
+//            )
+//        }
     }
 }
